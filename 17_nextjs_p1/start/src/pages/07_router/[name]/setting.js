730a43358b3939b143d5f07217e80b231e44dc4c
kaaisz <kaaisuzuki@gmail.com>
2023-08-24 05:51:19 +0900

    Try router.replace

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -4,7 +4,7 @@ export default function Setting({ query }) {
     const router = useRouter();
     console.log(query);
     const clickHandler = () => {
-        router.push('/', 'dummy-url') // clickHandler Triggers to replace / with dummy-url
+        router.replace('/', 'dummy-url') // clickHandler Triggers to replace / with dummy-url
     }
     return (
         <>
