56e9c65543431f8d68f5c533489e0dc81446acb3
kaaisz <kaaisuzuki@gmail.com>
2023-08-24 05:08:27 +0900

    Try Dynamic Routing #2: Use useRouter of module inside Next.js

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -1,8 +1,11 @@
+import { useRouter } from "next/router"
+
 export default function Setting({ query }) {
-    return <h1>{ query.name }</h1>
+    const router = useRouter();
+    console.log(router);
+    return <h1>Get name from router: { router.query.name }</h1>
 }
 
-// set function getServerSideProps(Funciton name must be exactly the same)
 export async function getServerSideProps({ query }) {
     return {
         props: { query }
